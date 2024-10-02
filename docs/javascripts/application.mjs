6863a4863a82dd55a2abe5b191d54e6a4ac921b7
Joe Lanman <joelanman@gmail.com>
2024-10-02 15:44:25 +0100

    remove unnecessary Universal Analytics code

--- docs/javascripts/application.mjs
+++ docs/javascripts/application.mjs
@@ -6,8 +6,7 @@ import { loadAnalytics } from './components/analytics.mjs'
 import CookieBanner from './components/cookie-banner.mjs'
 import {
   getConsentCookie,
-  isValidConsentCookie,
-  removeUACookies
+  isValidConsentCookie
 } from './components/cookie-functions.mjs'
 import CookiesPage from './components/cookies-page.mjs'
 
@@ -19,10 +18,6 @@ createAll(CookiesPage)
 const userConsent = getConsentCookie()
 if (userConsent && isValidConsentCookie(userConsent) && userConsent.analytics) {
   loadAnalytics()
-
-  // Remove UA cookies if the user previously had them set or Google attempts
-  // to set them
-  removeUACookies()
 }
 
 // Initialise cookie page
